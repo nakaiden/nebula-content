7c1b53ce4faa41bc4a455253d33127dc02dcef58
nakaiden <nakaiden@gmail.com>
2021-11-08 22:33:59 -0500

    initial comit

---
+++ data-type-notes.js
@@ -0,0 +1,11 @@
+let horseName = "Lucky Charm";
+let position = 3; 
+if(position === 1){
+    console.log(horseName,"finished in first place!")
+} else if(position === 2){
+    console.log(horseName,"finished in second place!")
+}else if(position === 3){
+    console.log(horseName,"finished in third place!")
+}else if(position >= 4){
+    console.log(horseName,"did not finish in the top three!")
+}
